756e943aa5182d528e7c44703fffb4a58aabd4a0
yyaremkiv <y.yaremkiv@gmail.com>
2023-04-30 16:37:07 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,19 @@ dotenv.config();
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin: 'http://localhost:3000',
-      credentials: true,
-    }),
-  );
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://www.example.com',
+      'http://app.example.com',
+      'https://example.com',
+      'https://www.example.com',
+      'https://app.example.com',
+    ],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
+
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
