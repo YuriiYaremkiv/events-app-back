5c34646740c09ade74799c11824e3f05382e4616
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-16 20:58:14 +0300

    change cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+// import * as cookieParser from 'cookie-parser';
+import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
