f8c6ae6c364b9e23b0b5b780fd9a607351374bbd
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-17 17:51:25 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as cookieParser from 'cookie-parser';
 import cookieParser = require('cookie-parser');
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
-  const server = express();
-
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
+  app.enableCors();
   // app.enableCors({
   //   origin: ['http://localhost:3000', 'https://events-app-front.vercel.app'],
-  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   //   credentials: true,
   // });
 
-  app.enableCors({
-    origin: 'https://events-app-front.vercel.app',
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: 'https://events-app-front.vercel.app',
+  //   allowedHeaders:
+  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  //   credentials: true,
+  // });
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
