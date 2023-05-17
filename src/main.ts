f55ef9956d0ea2e054483ddc1f044ecea38eb8e9
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-17 16:43:43 +0300

    change origin cors

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://events-app-front.vercel.app'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
-
   // app.enableCors({
-  //   origin: 'https://events-app-front.vercel.app',
-  //   allowedHeaders:
-  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  //   origin: ['http://localhost:3000', 'https://events-app-front.vercel.app'],
+  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   //   credentials: true,
   // });
+
+  app.enableCors({
+    origin: 'https://events-app-front.vercel.app',
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
