e2357bdfbf948ed7b51ca1723b042744c818e37c
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-17 17:00:15 +0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,22 @@ import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
 
-  // app.enableCors({
-  //   origin: ['http://localhost:3000', 'https://events-app-front.vercel.app'],
-  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  //   credentials: true,
-  // });
-
   app.enableCors({
-    origin: 'https://events-app-front.vercel.app',
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    origin: ['http://localhost:3000', 'https://events-app-front.vercel.app'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
+
+  // app.enableCors({
+  //   origin: 'https://events-app-front.vercel.app',
+  //   allowedHeaders:
+  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+  //   credentials: true,
+  // });
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
